d198cd3416ea16d10dcc06742870148f3cd5fad7
Anshuman Srivastava <AnshumanSrivastava@eaton.com>
2024-07-19 19:54:17 +0530

    wai

--- sf_objects/sf_deploy_qa.sql
+++ sf_objects/sf_deploy_qa.sql
@@ -1,2 +1,3 @@
 execute immediate from @tutorial/branches/main/sf_objects/GLD_FIN_DB/SCHEMAS/schemas.sql USING(DB => 'GLD_FIN_DB_QA');
+CALL SYSTEM$WAIT(2);
 execute immediate from @tutorial/branches/main/sf_objects/GLD_FIN_DB/TABLES/MARKVIEW/purchase_orders_report.sql USING(DB => 'GLD_FIN_DB_QA');
