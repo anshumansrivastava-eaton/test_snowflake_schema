863a90dab43ed1735afd785dc54e793afcee87b1
Anshuman Srivastava <AnshumanSrivastava@eaton.com>
2024-07-19 20:45:56 +0530

    wait

--- sf_objects/sf_deploy_dev.sql
+++ sf_objects/sf_deploy_dev.sql
@@ -1,3 +1,6 @@
 execute immediate from @tutorial/branches/main/sf_objects/GLD_FIN_DB/SCHEMAS/schemas.sql USING(DB => 'GLD_FIN_DB_DEV');
-CALL SYSTEM$WAIT(2);
+
+execute immediate from @tutorial/branches/main/sf_objects/GLD_FIN_DB/SCHEMAS/schemas.sql USING(DB => 'GLD_FIN_DB_DEV');
+SELECT * FROM INFORMATION_SCHEMA.SCHEMATA
+               WHERE SCHEMA_NAME = 'MARKVIEW'
 execute immediate from @tutorial/branches/main/sf_objects/GLD_FIN_DB/TABLES/MARKVIEW/purchase_orders_report.sql USING(DB => 'GLD_FIN_DB_DEV');
