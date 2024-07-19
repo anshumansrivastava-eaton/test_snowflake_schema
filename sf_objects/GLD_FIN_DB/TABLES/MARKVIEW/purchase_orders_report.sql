b7a65e7e6bc155ab4ca47a00e301c96bfd98271f
Anshuman Srivastava <AnshumanSrivastava@eaton.com>
2024-07-19 15:41:17 +0530

    adding database objects

---
+++ sf_objects/GLD_FIN_DB/TABLES/MARKVIEW/purchase_orders_report.sql
@@ -0,0 +1,15 @@
+CREATE OR ALTER TABLE markview.purchase_orders_report(
+  workitem_instance_id double, 
+  document_id double, 
+  po_number string, 
+  po_amount string, 
+  supplier_name string, 
+  supplier_number string, 
+  plant string, 
+  current_workflow_queue string, 
+  current_workflow_user string)
+ 
+ 
+
+
+; 
