47eb64c134ba481f0e7793c0366afcd0f9f2b636
Anshuman Srivastava <AnshumanSrivastava@eaton.com>
2024-07-19 19:26:37 +0530

    schemas

--- sf_objects/sf_deploy_qa.sql
+++ sf_objects/sf_deploy_qa.sql
@@ -1,1 +1,2 @@
+execute immediate from @tutorial/branches/main/sf_objects/GLD_FIN_DB/SCHEMAS/schemas.sql USING(DB => 'GLD_FIN_DB_QA');
 execute immediate from @tutorial/branches/main/sf_objects/GLD_FIN_DB/TABLES/MARKVIEW/purchase_orders_report.sql USING(DB => 'GLD_FIN_DB_QA');
