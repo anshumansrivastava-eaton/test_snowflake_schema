872d05221ac62b9902490724c6d8bbb7c01430ce
Anshuman Srivastava <AnshumanSrivastava@eaton.com>
2024-07-22 10:49:28 +0530

    schema

--- sf_objects/GLD_FIN_DB/TABLES/MARKVIEW/purchase_orders_report.sql
+++ sf_objects/GLD_FIN_DB/TABLES/MARKVIEW/purchase_orders_report.sql
@@ -7,7 +7,9 @@ CREATE OR ALTER TABLE {{DB}}.markview.purchase_orders_report(
   supplier_number string, 
   plant string, 
   current_workflow_queue string, 
-  current_workflow_user string)
+  current_workflow_user string,
+  test string
+  )
  
  
 
